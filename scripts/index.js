cd10ddad74e18407cc8d088c14dfca16f8f19247
Christopher D Couture <videogameroulette@yahoo.ca>
2021-06-18 16:23:03 -0700

    tourney update 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,11 @@ const websocket_endpoint = "wss://relay.aricodes.net/ws";
 
 var areaname = document.getElementById("areaname");
 var currenthp = document.getElementById("currenthp");
-var damage = document.getElementById("damage");
 var areaitems = document.getElementById("areaitems");
 var areascreens = document.getElementById("areascreens");
 var totalitems = document.getElementById("totalitems");
 var totalscreens = document.getElementById("totalscreens");
 var deathcount = document.getElementById("deathcount");
-var bubblecount = document.getElementById("bubblecount");
-var brickcount = document.getElementById("brickcount");
-var enemycount = document.getElementById("enemycount");
 
 window.onload = function () 
 {
@@ -35,15 +31,11 @@ function appendData(data)
 	console.log(data);
 	areaname.innerHTML = `Area Name: ${data.AreaName}`;
 	currenthp.innerHTML = `HP: ${data.CurrentHP}/${data.MaxHP}`;
-	damage.innerHTML = `Coat Damage Reduced: ${Math.floor(data.DamageReduction)}%`
 	areaitems.innerHTML = `Area Items: ${data.AreaItemPercent}%`;
 	areascreens.innerHTML = `Area Map: ${data.AreaMapPercent}%`;
 	totalitems.innerHTML = `Total Items: ${data.TotalItemPercent}%`;
 	totalscreens.innerHTML = `Total Map: ${data.TotalMapPercent}%`;
-	enemycount.innerHTML = `Total Enemies Glitched: ${data.TotalEnemiesPercent}%`;
 	deathcount.innerHTML = `Deaths: ${data.DeathCount}`;
-	bubblecount.innerHTML = `Bubbles Popped: ${data.BubbleCount}`;
-	brickcount.innerHTML = `Bricks Broken: ${data.BricksCount}`;
 }
 
 function ClearAll() {
